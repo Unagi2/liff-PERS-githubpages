f06fdbb7bedc92577b2cdfd27a1b953251610d17
Unagi_git <34627350+Unagi2@users.noreply.github.com>
2021-05-04 04:11:53 +0900

    display

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ $(function () {
     });
     */
 
+    //Display設定
+    // 要素を取得
+    let ele = document.getElementById('form-pass_first');
+    // 現在の display プロパティの値を保持
+    const displayOriginal = ele.style.display;
+    // none に設定して非表示
+    ele.style.display = 'none';
+    // 元に戻して表示
+    //ele.style.display = displayOriginal;
+
+
+
     // 送信
     $('form').submit(function () {
 
