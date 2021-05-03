5c5343f5c6588fca3d1246c2e59a827c6062265c
Unagi_git <34627350+Unagi2@users.noreply.github.com>
2021-05-04 06:16:51 +0900

    fix

--- index.js
+++ index.js
@@ -30,33 +30,40 @@ $(function () {
     let ele2 = document.getElementById('form-pass_second');
     let ele3 = document.getElementById('passwd2');
     let ele_bar02 = document.getElementById('bar02');
-    /*
+    
     let ele4 = document.getElementById('form-date');
     let ele5 = document.getElementById('date1');
-
+    let ele_bar03 = document.getElementById('bar03');
+    /*
     let ele6 = document.getElementById('form-time');
     let ele7 = document.getElementById('time1');
     let ele8 = document.getElementById('time2');
+    let ele_bar04 = document.getElementById('bar04);
 
     let ele9 = document.getElementById('form-temp');
     let ele10 = document.getElementById('temp');
+    let ele_bar05 = document.getElementById('bar05);
 
     let ele11 = document.getElementById('form-feverish');
     let ele12 = document.getElementById('feverish');
+    let ele_bar06 = document.getElementById('bar06);
 
     let ele13 = document.getElementById('form-respiratory');
     let ele14 = document.getElementById('respiratory');
     
     let ele16 = document.getElementById('check-respiratory');
+    let ele_bar07 = document.getElementById('bar07);
 
     let ele17 = document.getElementById('form-symptoms');
     let ele18 = document.getElementById('symptoms');
     let ele18 = document.getElementById('symptoms-text');
+    let ele_bar08 = document.getElementById('bar08);
     
     let ele20 = document.getElementById('contact');
     let ele21 = document.getElementById('contact-group');
     let ele22 = document.getElementById('contact-name');
     let ele23 = document.getElementById('contact-action');
+    let ele_bar09 = document.getElementById('bar09);
     */
 
     // 現在の display プロパティの値を保持
@@ -68,32 +75,39 @@ $(function () {
     const displayOriginal3 = ele3.style.display;
     const displayOriginal_bar02 = ele_bar02.style.display;
     
-    /*
+    
     const displayOriginal4 = ele4.style.display;
     const displayOriginal5 = ele5.style.display;
-
+    const displayOriginal_bar03 = ele_bar03.style.display;
+    /*
     const displayOriginal6 = ele6.style.display;
     const displayOriginal7 = ele7.style.display;
     const displayOriginal8 = ele8.style.display;
+    const displayOriginal_bar04 = ele_bar04.style.display;
 
     const displayOriginal9 = ele9.style.display;
     const displayOriginal10 = ele10.style.display;
+    const displayOriginal_bar05 = ele_bar05.style.display;
 
     const displayOriginal11 = ele11.style.display;
     const displayOriginal12 = ele12.style.display;
+    const displayOriginal_bar06 = ele_bar06.style.display;
 
     const displayOriginal13 = ele13.style.display;
     const displayOriginal14 = ele14.style.display;
 
     const displayOriginal16 = ele16.style.display;
+    const displayOriginal_bar07 = ele_bar07.style.display;
 
     const displayOriginal17 = ele17.style.display;
     const displayOriginal18 = ele18.style.display;
+    const displayOriginal_bar08 = ele_bar08.style.display;
 
     const displayOriginal20 = ele20.style.display;
     const displayOriginal21 = ele21.style.display;
     const displayOriginal22 = ele22.style.display;
     const displayOriginal23 = ele23.style.display;
+    const displayOriginal_bar09 = ele_bar09.style.display;
     */
 
     // none に設定して非表示
@@ -104,32 +118,39 @@ $(function () {
     ele2.style.display = 'none';
     ele3.style.display = 'none';
     ele_bar02.style.display = 'none';
-    /*
+    
     ele4.style.display = 'none';
     ele5.style.display = 'none';
-
+    ele_bar03.style.display = 'none';
+    /*
     ele6.style.display = 'none';
     ele7.style.display = 'none';
     ele8.style.display = 'none';
+    ele_bar04.style.display = 'none';
 
     ele9.style.display = 'none';
     ele10.style.display = 'none';
+    ele_bar05.style.display = 'none';
 
     ele11.style.display = 'none';
     ele12.style.display = 'none';
+    ele_bar06.style.display = 'none';
 
     ele13.style.display = 'none';
     ele14.style.display = 'none';
 
     ele16.style.display = 'none';
+    ele_bar07.style.display = 'none';
 
     ele17.style.display = 'none';
     ele18.style.display = 'none';
+    ele_bar08.style.display = 'none';
 
     ele20.style.display = 'none';
     ele21.style.display = 'none';
     ele22.style.display = 'none';
     ele23.style.display = 'none';
+    ele_bar09.style.display = 'none';
     */
 
     // 元に戻して表示
