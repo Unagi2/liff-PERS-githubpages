230de4aaaae57a9a7052730ffb3445d69d97f87a
Unagi_git <34627350+Unagi2@users.noreply.github.com>
2021-05-04 03:01:06 +0900

    time

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ $(function () {
         names = names.slice(0, -1);
         */
 
-        var msg = `初回利用：${first}\nPassword Set：${password_set}\nPassword：${password}\n記録日：${date}\n時間帯：${number}\n体温：${temp}\n熱感：${feverish}\n呼吸器症状：${respiratory}\n呼吸器症状内容：${respiratory_yes}\nその他自覚症状：${symptoms}\n自覚症状内容：${symptoms_yes}\n濃厚接触者：${contact}\n所属：${contact_group}\n名前：${contact_name}\n行動内容：${contact_action}`;
+        var msg = `初回利用：${first}\nPassword Set：${password_set}\nPassword：${password}\n記録日：${date}\n時間帯：${time}\n体温：${temp}\n熱感：${feverish}\n呼吸器症状：${respiratory}\n呼吸器症状内容：${respiratory_yes}\nその他自覚症状：${symptoms}\n自覚症状内容：${symptoms_yes}\n濃厚接触者：${contact}\n所属：${contact_group}\n名前：${contact_name}\n行動内容：${contact_action}`;
         sendText(msg);
 
         return false;
