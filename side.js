1c35b588955e4efde2c4c831ea1421dcef0df343
Unagi_git <34627350+Unagi2@users.noreply.github.com>
2021-05-07 16:59:12 +0900

    side js

---
+++ side.js
@@ -0,0 +1,30 @@
+$(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date_side"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+
+
+
+    // 送信
+    $('form[id = "submit_hospital"]').submit(function () {
+
+        var first = $('input[name="first_side"]:checked').val();
+        var password = $('input[id="passwd_side"]').val();
+        var date = $('input[name="date_side"]').val();
+        var where =$('textarea[name="where-text"]').val();
+        var what =$('textarea[name="what-text"]').val();
+
+
+        if(first == false | password == false | date == false | where == false | what == false ){
+            return false;
+        }
+
+        var msg = `【医療受診記録】\nmode:Hospital\n初回利用:${first}\nPassword:${password}\n受診日:${date}\n病院名:${where}\n病状:${what}\n`;
+        sendText(msg);
+
+        return false;
+    });
+});
