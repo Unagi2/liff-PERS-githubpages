974f1b049aba1f46f560d340e0958f02384a34de
Unagi_git <34627350+Unagi2@users.noreply.github.com>
2021-05-07 17:46:22 +0900

    fix badge

--- side.js
+++ side.js
@@ -11,18 +11,22 @@ $(function () {
     // 送信
     $('form').submit(function () {
 
-        var first = $('input[name="first_side"]:checked').val();
-        var password = $('input[id="passwd_side"]').val();
-        var date = $('input[name="date_side"]').val();
-        var where =$('textarea[name="where-text"]').val();
-        var what =$('textarea[name="what-text"]').val();
+        var badge = $('h1[name="badge').val();
+        if(badge == "医療受診記録"){
+            var first = $('input[name="first_side"]:checked').val();
+            var password = $('input[id="passwd_side"]').val();
+            var date = $('input[name="date_side"]').val();
+            var where =$('textarea[name="where-text"]').val();
+            var what =$('textarea[name="what-text"]').val();
 
 
-        if(first == false | password == false | date == false | where == false | what == false ){
-            return false;
+            if(first == false | password == false | date == false | where == false | what == false ){
+                return false;
+            }
+
+            var msg = `【医療受診記録】\nmode:Hospital\n初回利用:${first}\nPassword:${password}\n受診日:${date}\n病院名:${where}\n病状:${what}\n`;
         }
-
-        var msg = `【医療受診記録】\nmode:Hospital\n初回利用:${first}\nPassword:${password}\n受診日:${date}\n病院名:${where}\n病状:${what}\n`;
+        
         sendText(msg);
 
         return false;
